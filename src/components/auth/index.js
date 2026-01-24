19700faa3445ea8eff2f25be116fa61bf9a66ac4
Rakib Boshunia <fireaiboshunia.info@gmail.com>
2026-01-24 21:56:47 +0600

    delete otp

--- src/components/auth/index.js
+++ src/components/auth/index.js
@@ -3,7 +3,6 @@ export { default as AuthCard } from "./AuthCard";
 export { default as AuthHeader } from "./AuthHeader";
 export { default as TextInput } from "./TextInput";
 export { default as PasswordInput } from "./PasswordInput";
-export { default as OTPInput } from "./OTPInput";
 export { default as SubmitButton } from "./SubmitButton";
 export { default as BackNextButtons } from "./BackNextButtons";
 
